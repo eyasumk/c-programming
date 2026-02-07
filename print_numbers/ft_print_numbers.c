2b38f9d7ea99f5b07c07a0ece00f6d90edefd836
Eyasu Mulugeta <jobs.eyasu@gmail.com>
2026-02-07 23:22:25 +0100

    Completed the print_numbers function body.

--- print_numbers/ft_print_numbers.c
+++ print_numbers/ft_print_numbers.c
@@ -6,11 +6,19 @@
 /*   By: emuluget <emuluget@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/07 23:14:49 by emuluget          #+#    #+#             */
-/*   Updated: 2026/02/07 23:16:04 by emuluget         ###   ########.fr       */
+/*   Updated: 2026/02/07 23:20:53 by emuluget         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 void ft_print_numbers(void);
 
 void ft_print_numbers(void)
-{}
+{
+	char num;
+	num = 48;
+	while (num <= 57)
+	{
+		write(1, &num, 1);
+		num++;
+	}
+}
