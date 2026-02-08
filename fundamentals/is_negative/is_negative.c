3b1ecc94503814186f9215b589c0e5f66eb4c6b6
Eyasu Mulugeta <jobs.eyasu@gmail.com>
2026-02-08 23:42:42 +0100

    Completed the the custom implementation of the C programming is_negative() function.

--- fundamentals/is_negative/is_negative.c
+++ fundamentals/is_negative/is_negative.c
@@ -6,7 +6,7 @@
 /*   By: emuluget <emuluget@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/08 23:38:25 by emuluget          #+#    #+#             */
-/*   Updated: 2026/02/08 23:39:32 by emuluget         ###   ########.fr       */
+/*   Updated: 2026/02/08 23:41:29 by emuluget         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,17 @@ void ft_is_negative(int n);
 
 void ft_is_negative(int n)
 {
-	
+	char	positive;
+	char	negative;
+
+	positive = 'P';
+	negative = 'N';
+	if (n > 0 || n == 0)
+	{
+		write(1, &positive, 1);
+	}
+	else
+	{
+		write(1, &negative, 1);
+	}
 }
