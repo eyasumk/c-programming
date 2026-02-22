fe5927fcf1016c28fca59e36c81de4aeaae3e544
Eyasu Mulugeta <jobs.eyasu@gmail.com>
2026-02-22 23:44:57 +0100

    Renamed the directory to strcapitalize

---
+++ fundamentals/strcapitalize/ft_strcapitalize.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcapitalize.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: emuluget <emuluget@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/12 12:36:40 by emuluget          #+#    #+#             */
+/*   Updated: 2025/08/12 21:57:20 by emuluget         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_str_to_lowcase(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 'A' && str[i] <= 'Z')
+			str[i] = str[i] + 32;
+		i++;
+	}
+	return (str);
+}
+
+char	*ft_strcapitalize(char *str)
+{
+	int		i;
+	int		j;
+
+	i = 0;
+	j = i + 1;
+	ft_str_to_lowcase(str);
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			if (j == 1)
+				str[i] -= 32;
+			j = 0;
+		}
+		else if (str[i] >= '0' && str[i] <= '9')
+			j = 0;
+		else
+			j = 1;
+		i++;
+	}
+	return (str);
+}
